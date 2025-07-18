a332eb84e1f54034dc7d91f1d5086ca755883c55
Jaroslav Petřík <jarek.petrik@proton.me>
2025-07-18 21:49:25 +0200

    Add files via upload

---
+++ js/produkty.js
@@ -0,0 +1,89 @@
+/// KATEGORIE JÍDLA ///
+
+const kategorieJidla = [
+
+    {
+        name: "zelenina",
+        niceName: "Zelenina"
+    },
+    {
+        name: "ovoce",
+        niceName: "Ovoce"
+    },
+    {
+        name: "pecivo",
+        niceName: "Pečivo"
+    },
+    {
+        name: "sladkosti",
+        niceName: "Sladkosti"
+    },
+
+];
+
+///Zelenina///
+
+const zelenina = [
+    {
+        name: "mrkev",
+        niceName: "Mrkev",
+        jednotka: "g",
+        mnozstvi: 100,
+        phe: 40
+    },
+    {
+        name: "cesnek",
+        niceName: "Česknek",
+        jednotka: "g",
+        mnozstvi: 100,
+        phe: 214
+    },
+    {
+        name: "cibule",
+        niceName: "Cibule",
+        jednotka: "g",
+        mnozstvi: 100,
+        phe: 35
+    },
+    {
+        name: "brambory",
+        niceName: "Brambory",
+        jednotka: "g",
+        mnozstvi: 100,
+        phe: 100
+    }
+
+];
+
+///Ovoce///
+const ovoce = [
+    {
+        name: "jablko",
+        niceName: "Jablko",
+        jednotka: "g",
+        mnozstvi: 100,
+        phe: 22
+    }
+]
+
+///Pečivo///
+const pecivo = [
+    {
+        name: "nbrohlik",
+        niceName: "NB Rohlík",
+        jednotka: "ks",
+        mnozstvi: 1,
+        phe: 10
+    }
+]
+
+///Sladkosti ///
+const sladkosti = [
+    {
+        name: "krupinka",
+        niceName: "Křupinka",
+        jednostka: "ks",
+        mnozstvi: 1,
+        phe: 10
+    }
+];
