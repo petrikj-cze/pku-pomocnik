46ecf9d532ac80aa872393576ea63eee225b57e0
Jaroslav Petřík <jarek.petrik@proton.me>
2025-07-18 21:51:43 +0200

    Aktuální verze
    Nahrání aktuální verze pro možnost pokračování.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+//Načítání prvků
+
+const kategorieSelect = document.getElementById('category');
+const jidloSelect = document.getElementById('food-item');
+const gramyJidla = document.getElementById('grams');
+const celkemPheJidlo = document.getElementById('count');
+
+
+///////////////////////////////////
+
+///Inicializace
+
+window.addEventListener("load", function(){
+    kategorieSelect.innerHTML += "<option>asdf</option>";
+});
+
+///////////////////////////////////
+
+
+
 function drawPieChart(maximum, aktualni_hodnota) {
     const canvas = document.querySelector("#pieChart")
     
@@ -38,4 +58,72 @@ function drawPieChart(maximum, aktualni_hodnota) {
     ctx.fill();
 
     document.querySelector("#aktualniphe").innerText = aktualni_hodnota + " / " + maximum + " pHe";
+}
+
+kategorieSelect.addEventListener("change", function(){ //vymaze všechny optiony v selectu výběru kategorie a načte související s vybranou kategorií pomocí funkce nactiJidlaKategorie(kategorie)
+   
+    jidloSelect.innerHTML = "";
+
+    nactiJidlaKategorie(kategorieSelect.value);
+
+});
+
+jidloSelect.addEventListener("change", function(){
+    const label = document.getElementById("label-grams");
+    const InfoJednotkaPhe = document.getElementById("infoJednotkaPhe");
+    const jednotka = nactiInformace(jidloSelect.value).jednotka;
+    InfoJednotkaPhe.innerHTML = "V " + nactiInformace(jidloSelect.value).mnozstvi + " " + nactiInformace(jidloSelect.value).jednotka + " je " + nactiInformace(jidloSelect.value).phe + " Phe"; 
+    label.innerHTML = "Množství (" + jednotka + ")";
+});
+
+
+gramyJidla.addEventListener("change", function(){
+
+    const numberGramyJidla = Number(gramyJidla.value); //Gramy zadané uživatelem
+    const infoJidlo = nactiInformace(jidloSelect.value); //Informace o konkrétní surovině
+    const gramySuroviny = Number(infoJidlo.mnozstvi);
+    const phe = Number(infoJidlo.phe);
+
+
+    celkemPheJidlo.value = (numberGramyJidla / gramySuroviny) * phe;
+});
+
+function nactiInformace(jidlo){
+    
+    if(kategorieSelect.value == "zelenina"){
+        return zelenina.find(item => item.name === jidlo);
+     } else if(kategorieSelect.value == "ovoce"){
+        return ovoce.find(item => item.name === jidlo);
+     } else if(kategorieSelect.value == "pecivo"){
+        return pecivo.find(item => item.name === jidlo);
+     } 
+
+
+}
+
+function nactiJidlaKategorie(kategorie){
+    const category = kategorie;
+
+    if(kategorieSelect.value == "zelenina"){
+        jidloSelect.innerHTML += `<option value="">Vyberte jídlo...</option>`;
+        for(const zel of zelenina){
+            jidloSelect.innerHTML += `<option value="${zel.name}">${zel.niceName}</option>`;
+    } 
+     } else if(kategorieSelect.value == "ovoce"){
+        jidloSelect.innerHTML += `<option value="">Vyberte jídlo...</option>`;
+        for(const ov of ovoce){
+            jidloSelect.innerHTML += `<option value="${ov.name}">${ov.niceName}</option>`;
+    } 
+     } else if(kategorieSelect.value == "pecivo"){
+        jidloSelect.innerHTML += `<option value="">Vyberte jídlo...</option>`;
+        for(const pec of pecivo){
+            jidloSelect.innerHTML += `<option value="${pec.name}">${pec.niceName}</option>`;
+    } 
+     } else if(kategorieSelect.value == "sladkosti"){
+        jidloSelect.innerHTML += `<option value="">Vyberte jídlo...</option>`;
+        for(const slad of sladkosti){
+            jidloSelect.innerHTML += `<option value="${slad.name}">${slad.niceName}</option>`;
+    } 
+     } 
+
 }
